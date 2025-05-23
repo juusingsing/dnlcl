0bd25799f60aa922748888b27f201cc984fcf6b1
juusingsing <ansjs1@naver.com>
2025-05-23 15:20:09 +0900

    1111

--- app/src/main/java/com/pj4/MainActivity.java
+++ app/src/main/java/com/pj4/MainActivity.java
@@ -10,11 +10,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    // WebView와 관련된 기능을 관리하는 객체
+    private WebViewManager webViewManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // WebView와 관련된 작업을 관리할 WebViewManager 객체 초기화
+        // activity_main.xml 레이아웃에서 webview 요소를 찾아서 WebViewManager에 전달
+        webViewManager = new WebViewManager(this, findViewById(R.id.webview));
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
